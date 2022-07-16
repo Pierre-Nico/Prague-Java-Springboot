ee7f09203162f8ab54ea01a9abe32fa910a3c058
pgerm <pgermain974@icloud.com>
2022-07-16 17:43:21 +0200

    First Commit

---
+++ src/main/java/com/example/PragueSpringBoot/PragueSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.PragueSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PragueSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PragueSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PragueSpringBoot/PragueSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PragueSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PragueSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
